ab09bbd63022785f76da00175773f49af009f33e
kevinagyeman <kevinagyemann@gmail.com>
2026-07-06 22:26:14 +0200

    lint: remove white line

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -30,7 +30,6 @@ export const LANGUAGES = LOCALE_CODES.map((code) => ({
 // Zod-compatible tuple: z.enum(LOCALE_ENUM)
 export const LOCALE_ENUM = LOCALE_CODES as unknown as [Locale, ...Locale[]];
 
-
 export const LANGUAGE_LABELS: Record<string, string> = Object.fromEntries(
 	LOCALE_CODES.map((code) => [code, code]),
 );
